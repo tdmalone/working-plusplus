6788ee1f5fd2034a45906ad8523f28a5b2933e83
Tim Malone <tdmalone@gmail.com>
2018-08-08 21:07:35 +1000

    Additional tests for isValidEvent

--- tests/app.js
+++ tests/app.js
@@ -11,7 +11,59 @@
 
 const app = require( '../src/app' );
 
+/**
+ * IsValidEvent.
+ */
+
+test( 'Event with message and text is reported as valid', () => {
+  const payload = {
+    type: 'message',
+    text: 'Hello'
+  };
+
+  expect( app.isValidEvent( payload ) ).toBe( true );
+});
+
 test( 'Event with missing type is caught as invalid', () => {
-  const payload = {};
+  const payload = {
+    text: 'Hello'
+  };
+
+  expect( app.isValidEvent( payload ) ).toBe( false );
+});
+
+test( 'Event with non-message type is caught as invalid', () => {
+  const payload = {
+    type: 'random',
+    text: 'Hello'
+  };
+
+  expect( app.isValidEvent( payload ) ).toBe( false );
+});
+
+test( 'Event with a subtype is caught as invalid', () => {
+  const payload = {
+    type:    'message',
+    subtype: 'random',
+    text:    'Hello'
+  };
+
+  expect( app.isValidEvent( payload ) ).toBe( false );
+});
+
+test( 'Event without text set is caught as invalid', () => {
+  const payload = {
+    type: 'message'
+  };
+
+  expect( app.isValidEvent( payload ) ).toBe( false );
+});
+
+test( 'Event with only a space as text is caught as invalid', () => {
+  const payload = {
+    type: 'message',
+    text: ' '
+  };
+
   expect( app.isValidEvent( payload ) ).toBe( false );
 });
