15a0098b9379be5ed15df9b8c04e2e89cd0a9ed4
MattLParker <MLParker1@gmail.com>
2018-12-13 19:02:18 -0500

    added =

--- src/helpers.js
+++ src/helpers.js
@@ -62,7 +62,7 @@ const extractCommand = ( message, commands ) => {
  */
 const extractPlusMinusEventData = ( text ) => {
 
-  const data = text.match( /@([A-Za-z0-9:-_]+?)>?\s*(\+{2}|-{2}|—{1})/ );
+  const data = text.match( /@([A-Za-z0-9:-_]+?)>?\s*(\+{2}|-{2}|—{1}|={2})/ );
 
 
   if ( ! data ) {
