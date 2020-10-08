07f0e82551d06fe7b71a88e2b4da0b91eaa868ad
Martin Kenjic <martin.kenjic@agiledrop.com>
2020-10-08 10:38:09 +0200

    Add date, channel filters, list search

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -1,51 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import logo from '../logo.svg';
-import DateRangePicker from '@wojtekmaj/react-daterange-picker'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  Button,
+  ButtonGroup,
+  ButtonDropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle
+} from 'reactstrap';
+
 
 const Chart = (props) => {
 
-  // const apiURL = 'https://04933df8c7b9.eu.ngrok.io/leaderboard' + props.location.search;
-
   // id | user_receive | user_give | timestamp | channel
-  const [users2, setUsers2] = useState([
-    {id: 1, user_receive: "Benjamin Cizej", user_give: "Martin Kenjic",  timestamp: "1597233600", channel: "#general" },
-    {id: 2, user_receive: "Benjamin Cizej", user_give: "Martin Kenjic",  timestamp: "1597233600", channel: "#general" },
-    {id: 3, user_receive: "Klemen Brodej", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 4, user_receive: "Bostjan Kovac", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 5, user_receive: "Klemen Brodej", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 6, user_receive: "Bostjan Kovac", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 7, user_receive: "Klemen Brodej", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 8, user_receive: "Klemen Brodej", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 9, user_receive: "Klemen Brodej", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 10, user_receive: "Klemen Brodej", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 11, user_receive: "Janez Novak", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 12, user_receive: "Janez Novak", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 13, user_receive: "Janez Novak", user_give: "Martin Kenjic",  timestamp: "1600527600", channel: "#general" },
-    {id: 14, user_receive: "", user_give: "Martin Kenjic",  timestamp: "1597233600", channel: "#general" },
-  ]);
-
   const [users, setUsers] = useState([
-    {rank: 1, item: "Benjamin Cizej", score: "40 points"},
-    {rank: 2, item: "Klemen Brodej", score: "30 points"},
-    {rank: 3, item: "Martin Kenjic", score: "20 points"},
-    {rank: 4, item: "Bostjan Kovac", score: "10 points"},
-    {rank: 5, item: "Benjamin Cizej", score: "9 points"},
-    {rank: 6, item: "Klemen Brodej", score: "8 points"},
-    {rank: 7, item: "Martin Kenjic", score: "7 points"},
-    {rank: 8, item: "Bostjan Kovac", score: "6 points"},
+    {id: 3, user_receive: "Klemen Brodej", user_give: "Martin Kenjic", timestamp: "1600527600", channel: "#dev" },
+    {id: 4, user_receive: "Bostjan Kovac", user_give: "Martin Kenjic", timestamp: "1600527600", channel: "#dev" },
+    {id: 5, user_receive: "Klemen Brodej", user_give: "Martin Kenjic", timestamp: "1600527600", channel: "#dev" },
+    {id: 6, user_receive: "Bostjan Kovac", user_give: "Martin Kenjic", timestamp: "1601337600", channel: "#dev" },
+    {id: 7, user_receive: "Klemen Brodej", user_give: "Martin Kenjic", timestamp: "1600527600", channel: "#dev" },
+    {id: 8, user_receive: "Klemen Brodej", user_give: "Martin Kenjic", timestamp: "1600527600", channel: "#dev" },
+    {id: 9, user_receive: "Klemen Brodej", user_give: "Martin Kenjic", timestamp: "1600527600", channel: "#dev" },
+    {id: 10, user_receive: "Klemen Brodej", user_give: "Martin Kenjic", timestamp: "1601337600", channel: "#dev" },
+    {id: 11, user_receive: "Janez Novak", user_give: "Martin Kenjic", timestamp: "1601337600", channel: "#dev" },
+    {id: 12, user_receive: "Janez Novak", user_give: "Martin Kenjic", timestamp: "1601337600", channel: "#dev" },
+    {id: 13, user_receive: "Janez Novak", user_give: "Martin Kenjic", timestamp: "1600527600", channel: "#dev" },
+    {id: 14, user_receive: "Lena Gregorcic", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#dev" },
+    {id: 15, user_receive: "Lena Gregorcic", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#dev" },
+    {id: 14, user_receive: "Lena Gregorcic", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#dev" },
+    {id: 16, user_receive: "Lena Gregorcic", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#dev" },
+    {id: 17, user_receive: "John Smith", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#random" },
+    {id: 18, user_receive: "John Smith", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#random" },
+    {id: 19, user_receive: "John Smith", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#random" },
+    {id: 20, user_receive: "John Smith", user_give: "Martin Kenjic", timestamp: "1601596800", channel: "#random" },
   ]);
-  const [searchTerm, setSearchTerm] = useState('');
 
-  const unixTime = 1600607620; //Math.floor( Date.now() / 1000 );
-  const unixTime2 = 1597233600;
-  //const date = new Date(unixTime2 * 1000);
-
-  // prevMonth.setDate(1);
-  // prevMonth.setMonth(prevMonth.getMonth()-1);
-
-
-  //console.log(between(unixTime2, prevMonthFirst, prevMonthLast));
+  // const apiURL = 'https://04933df8c7b9.eu.ngrok.io/leaderboard' + props.location.search;
 
   // useEffect(() => {
   //   const getChart = async() => {
@@ -57,193 +53,194 @@ const Chart = (props) => {
   //   }
   //   getChart();
 
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   // eslint-disable-next-line
   // }, []);
 
-  const handleChange = e => {
-    setSearchTerm(e.target.value);
-  };
-
-  const date = new Date();
-  const date2 = new Date();
-
-  let firstDay = (date.getDate() - date.getDay() - 7) + 1;
-  let lastDay = firstDay + 6;
-
-  let first = new Date(date.setDate(firstDay));
-  let last = new Date(date2.setDate(lastDay));
-
-  let first1 = new Date(date.getFullYear(), date.getMonth()-1, 1);
-
-  console.log(firstDay);
-  console.log(first1 + '\n' + last);
-
-  const inRange = (mapArray, range = 'month') => {
+  const inRange = (mapArray, range = 'allTime', channel) => {
     const date = new Date();
-    const date2 = new Date();
-
+    let ranks = {};
     let first;
     let last;
 
-    if (range === 'month') {
+    if (range === 'allTime') {
+      first = new Date(0).getTime() / 1000;
+      last = new Date().getTime() / 1000;
+    } else if (range === 'lastMonth') {
       first = new Date(date.getFullYear(), date.getMonth()-1, 1).getTime() / 1000;
       last = new Date(date.getFullYear(), date.getMonth(), 0).getTime() / 1000;
-    } if (range === 'week') {
-      let firstDay = (date.getDate() - date.getDay() - 7) + 1;
-      let lastDay = firstDay + 6;
-      let first = new Date(date.setDate(firstDay)).toUTCString();
-      let last = new Date(date2.setDate(lastDay)).toUTCString();
+    } else if (range === 'lastWeek') {
+      const firstDay = date.getDate() - date.getDay() - 7 + 1;
+      const lastDay = firstDay + 7;
+      first = new Date( date.getFullYear(), date.getMonth(), firstDay ).getTime() / 1000;
+      last = new Date( date.getFullYear(), date.getMonth(), lastDay ).getTime() / 1000;
+    } else if (range === 'thisMonth') {
+      first = new Date(date.getFullYear(), date.getMonth(), 1).getTime() / 1000;
+      last = new Date().getTime() / 1000;
+    } else if (range === 'thisWeek') {
+      const firstDay = date.getDate() - date.getDay() + 1;
+      first = new Date( date.getFullYear(), date.getMonth(), firstDay ).getTime() / 1000;
+      last = new Date().getTime() / 1000;
     }
     
     const results = mapArray.filter(user => {
       if(user.timestamp >= first && user.timestamp <= last) {
-          return user;
+          if(user.channel === channel) {
+            return user;
+          }
         }
     })
 
-    return results;
+    results.forEach(el => {
+      ranks[el.user_receive] = (ranks[el.user_receive] || 0) + 1;
+    })
+
+    let rangeresult = Object.keys(ranks).map(e => ({user_receive: e, score: ranks[e]}));
+
+    rangeresult.sort((a, b) => {
+      return b.score - a.score;
+    });
+
+    rangeresult.forEach((item, i) => {
+      item.rank = i + 1;
+    });
+
+    return rangeresult;
+
   }
 
-  const range = inRange(users2, 'month');
-  //console.log(range);
 
-  let ranks = {};
-
-  range.forEach(el => {
-      ranks[el.user_receive] = (ranks[el.user_receive] || 0) + 1;
+  const channels = users.map(el => {
+    return el.channel;
   })
+  const distinctChannels = [ ...new Set(channels) ];
 
-  let rangeresult = Object.keys(ranks).map(e => ({user_receive: e, score: ranks[e]}))
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isActive, setIsActive] = useState('allTime');
 
-  rangeresult.sort((a, b) => {
-    return b.score - a.score;
-  });
+  const [filterChannel, setFilterChannel] = useState('#dev');
+  const [range, setRange] = useState(inRange(users, isActive, filterChannel));
 
-  rangeresult.forEach((item, i) => {
-    item.rank = i + 1;
-  });
+  const handleChange = e => setSearchTerm(e.target.value);
 
-  console.log(rangeresult);
+  const filterDates = (dates, channel) => {
+    setRange(inRange(users, dates, channel));
+    setIsActive(dates);
+    setSearchTerm('');
+  }
 
-  // console.log(ranks);
-
-  // const map = range.reduce((acc, e) => acc.set(e.user_receive, (acc.get(e.user_receive) || 0) + 1), new Map());
-
-  // console.log(map);
-
-  // console.info([...map.keys()])
-  // console.info([...map.values()])
-
-  // const keys = [...map.keys()];
-  // const values = [...map.values()];
-  // const merged = keys.reduce((obj, key, index) => ({ ...obj, [key]: values[index] }), {});
-
-  // var array = keys.map((el, i) => {
-  //   return [keys[i], values[i]];
-  // });
-
-
-  // console.log(array);
-  
-
-
-  //console.log(ranks);
-
-    // ranks.push({ user_receive: el.user_receive, points: ranks[el.user_receive] = (ranks[el.user_receive] || 0) + 1 });
-    // ranks[el.user_receive] = (ranks[el.user_receive] || 0) + 1;
-    // if (el.user_receive = el.user_receive) {
-    //   console.log(el.user_receive || 0) + 1);
-    //   //ranks[el.user_receive] = (ranks[el.user_receive] || 0) + 1;
-    // }
-
-  // const countVotes = range.filter(user => {
-  //   return user;
-  // })
-  //console.log(range)
-  
-
-  const results = !searchTerm ? rangeresult : rangeresult.filter(user =>
+  const results = !searchTerm ? range : range.filter(user =>
     user.user_receive.toLowerCase().includes(searchTerm.toLocaleLowerCase())
   );
 
-  //console.log(results);
+  const pluralize = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
 
-  // const between = (x, min, max) => {
-  //   return x >= min && x <= max;
-  // }
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-  // users2.map(user => {
-  //   if(between(user.timestamp, prevMonthFirst, prevMonthLast)) {
-  //     console.log(user)
-  //   }
-  // });
-  const [value, onChange] = useState([new Date(), new Date()]);
-  //console.log(value);
+  const [dropdownOpen, setOpen] = useState(false);
+  const toggleDropDown = () => setOpen(!dropdownOpen);
 
   return(
     <>
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container">
-          <img src={logo} alt="Agiledrop" />
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+    <Navbar light expand="md">
+      <div className="container">
+        <NavbarBrand href="/"><img src={logo} alt="Agiledrop" /></NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Last Month</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Last Week</a>
-              </li>
-            </ul>
-            <form className="form-inline my-2 my-lg-0">
-              <input
-              className="form-control mr-sm-2"
+            <ButtonGroup>
+              <Button className={`${isActive === 'thisWeek' ? 'active' : ''} btn btn-light`} onClick={() => filterDates('thisWeek', filterChannel)}>This Week</Button>
+              <Button className={`${isActive === 'thisMonth' ? 'active' : ''} btn btn-light`} onClick={() => filterDates('thisMonth', filterChannel)}>This Month</Button>
+              <Button className={`${isActive === 'lastWeek' ? 'active' : ''} btn btn-light`} onClick={() => filterDates('lastWeek', filterChannel)}>Last Week</Button>
+              <Button className={`${isActive === 'lastMonth' ? 'active' : ''} btn btn-light`} onClick={() => filterDates('lastMonth', filterChannel)}>Last Month</Button>
+              <Button className={`${isActive === 'allTime' ? 'active' : ''} btn btn-light`} onClick={() => filterDates('allTime', filterChannel)}>All Time</Button>
+              <ButtonDropdown isOpen={dropdownOpen} toggle={toggleDropDown}>
+                <DropdownToggle caret>
+                  Channels
+                </DropdownToggle>
+                <DropdownMenu>
+                  {distinctChannels ? (distinctChannels.map((el, index) => (
+                    <DropdownItem key={index} onClick={() => { setFilterChannel(el); filterDates(isActive, el) }}>{el}</DropdownItem>
+                    )
+                  )) : null}
+                </DropdownMenu>
+              </ButtonDropdown>
+            </ButtonGroup>
+
+          </Nav>
+        </Collapse>
+        </div>
+      </Navbar>
+      <div className="container pt-5 pb-5">
+        <div className="row">
+        <div className="col">
+          <div className="card-deck">
+            {range ? (range.slice(0, 3).map(user => (
+                <div className={`${user.rank === 1 ? 'first' : user.rank === 2 ? 'second' : user.rank === 3 ? 'third' : ''} card text-center`} key={user.rank}>
+                    { user.rank === 1 ? <div className="podium"><span role="img" aria-label="1">🥇</span></div> :
+                      user.rank === 2 ? <div className="podium"><span role="img" aria-label="2">🥈</span></div> :
+                      user.rank === 3 ? <div className="podium"><span role="img" aria-label="3">🥉</span></div> :
+                      null
+                    }
+                    <h4>{user.user_receive}</h4>
+                    <div className="card-footer score">
+                      {pluralize(user.score, 'Point')}
+                    </div>
+                </div>
+              ))) : null }
+          </div>
+        </div>
+        </div>
+        {(results === undefined || results.length === 0) ?
+        
+        <div className="row mt-5">
+          <div className="col text-center">
+            <h1>No karma given yet!</h1>
+            <p>Be the first to give some karma points on slack.</p>
+          </div>
+        </div>
+
+        :
+        
+        <div className="row mt-5">
+        <div className="col-6">
+          <h3 className="pb-3">Karma List</h3>
+        </div>
+        <div className="col-6">
+            <input
+              className="form-control"
               type="text"
               placeholder="Search"
               aria-label="Search"
               value={searchTerm}
               onChange={handleChange}
             />
-            </form>
-          </div>
         </div>
-      </nav>
-      <div className="container mt-5">
-        <div className="card">
-          <div className="card-body">
-            <div className="btn-group" role="group" aria-label="Basic example">
-              <button type="button" className="btn btn-secondary">Last Week</button>
-              <button type="button" className="btn btn-secondary">Last Month</button>
-            </div>
-            <div className="table-responsive">
-            <table className="table table-borderless table-hover">
+        <div className="col">
+          <div className="table-responsive">
+            <table className="table table-borderless table-striped">
               <thead>
                 <tr>
                   <th scope="col" className="text-left">Rank</th>
                   <th scope="col" className="text-center">Name</th>
-                  <th scope="col" className="text-right">Points</th>
+                  <th scope="col" className="text-right">Karma</th>
                 </tr>
               </thead>
               <tbody>
-              {results ? (results.map(user => (
+               {(results.map(user => (
                   <tr key={user.rank}>
-                      { user.rank === 1 ? <th className='text-left podium' scope='row'><span role="img" aria-label="1">🥇</span></th> :
-                        user.rank === 2 ? <th className="text-left podium" scope="row"><span role="img" aria-label="2">🥈</span></th> :
-                        user.rank === 3 ? <th className="text-left podium" scope="row"><span role="img" aria-label="3">🥉</span></th> :
-                        <th className="text-left" scope="row">{user.count}</th>
-                      }
+                    <th className="text-left" scope="row">{user.rank}</th>
                     <td className="text-center">{user.user_receive}</td>
-                    <td className="text-right">{user.score}</td>
+                    <td className="text-right">{pluralize(user.score, 'Point')}</td>
                   </tr>
-                ))) : null }
+                )))}
               </tbody>
             </table>
-            </div>
           </div>
         </div>
+        </div>
+        }
       </div>
     </>
   )
